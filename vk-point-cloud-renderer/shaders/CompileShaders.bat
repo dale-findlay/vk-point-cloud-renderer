bce4c8fc92710671b9d17d74dc3dd25a23dbfae1
Dale Findlay <dalef@red-wild.com>
2020-10-05 10:55:15 +1000

    Added Vulkan abstractions

--- vk-point-cloud-renderer/shaders/CompileShaders.bat
+++ vk-point-cloud-renderer/shaders/CompileShaders.bat
@@ -1,3 +1,4 @@
-%VULKAN_SDK%/Bin32/glslc.exe shader.vert -o vert.spv
-%VULKAN_SDK%/Bin32/glslc.exe shader.frag -o frag.spv
+%VULKAN_SDK%/Bin32/glslc.exe renderDepth.comp -o renderDepth.spv
+%VULKAN_SDK%/Bin32/glslc.exe renderAttribute.comp -o renderAttribute.spv
+%VULKAN_SDK%/Bin32/glslc.exe resolve.comp -o resolve.spv
 pause
---
+++ vk-point-cloud-renderer/src/core/GameObject.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace vkpc {
+	class GameObject {	
+		
+	};
+}
